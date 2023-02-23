c048959b58befa89043ce23b242b85080d7cebbf
Jonathan <jono_95@yahoo.com>
2023-02-23 15:52:48 -0600

    commit tree 902327a5f4a2758c23deca5e3bd4632e02daccd1

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list
+    list = []
+        self.characters.each do |person|
+            list << "#{person.actor.first_name} #{person.actor.last_name}"
+        end
+    return list
+    end
 end
